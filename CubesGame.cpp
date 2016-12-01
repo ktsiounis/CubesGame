71e1ac43cba6e0c1f87e8ef5253c628f5e446591
ktsiounis <ntinos.tsi199@gmail.com>
2016-12-01 18:39:22 +0200

    Last changes

--- CubesGame.cpp
+++ CubesGame.cpp
@@ -14,11 +14,9 @@
 int N;
 // actual vector representing the camera's direction
 int lx=0,lz=-1;
-// XZ position of the camera
+// XZY position of the camera
 int x=0, z=5, y=1;
-int temp;
-int temp1;
-int layer;
+int temp,cubesCounter,layer;
 
 
 //I use struct for player
@@ -33,18 +31,15 @@ Player player = {3, 50, 0};
 class cube{
 	private:
 		int colour;
-		int status;
-		int posX, posY, posZ;
-		int id;
+		int xpos,ypos,zpos;
 	public:
-		cube() { status = 0; colour = 0; }
+		cube() { colour = 0; }
 		void setColour() { colour = (1+(rand() % 4)); }
 		void setSpecificColour(int c) {colour = c;}
 		int getColour() {return colour;}
-		int getStatus() {return status;}
 		void eraseCube() {colour =0;}
 };
-cube ***C;
+cube ***C; //initialiaze an 3D array of cubes
 
 //allocate memory for the cubes
 void initializeCubeArray(){
@@ -57,19 +52,20 @@ void initializeCubeArray(){
 	}
 	for(int i=0; i<N; i++) {//Set colours to the first layer's cubes
 		for(int j=0; j<N; j++) {
-			C[i][j][0].setColour(); //x,z,y y is the layers
+			C[i][j][0].setColour(); //x,z,y y are the layers
 		}
 	}
 }
 
 void collapseAllCubes(){
-	layer = y;
-	while(layer < N){
+	layer = y; //set the current layer(y) as layer so as to change it without problems
+	while(layer < N){ //do it for every layer
 		for(int i=0; i<N; i++){
 			for(int j=0; j<N; j++){
-				if(C[i][j][layer-1].getColour() == 0 && C[i][j][layer].getColour() != 0){
-					int step = 1;
-					while(C[i][j][layer-step].getColour() == 0  && C[i][j][layer-step+1].getColour() != 0){
+				//check every cube on this layer
+				if(C[i][j][layer-1].getColour() == 0 && C[i][j][layer].getColour() != 0){//if the current cube has a colour and under him
+					int step = 1;														 //there is nothing
+					while(C[i][j][layer-step].getColour() == 0  && C[i][j][layer-step+1].getColour() != 0){//drop this cube until you find the next cube under him
 						C[i][j][layer-step].setSpecificColour(C[i][j][layer-step+1].getColour());
 						C[i][j][layer-step+1].eraseCube();
 						step++;
@@ -99,7 +95,7 @@ void checkTheBoxUnderMe(){//This function checks what's underneath player so as 
 //This function draws a cube
 void drawcube(int x, int y, int z, int c){
 	
-	switch(c)
+	switch(c) //check the cube's colour and set the appropriate colour
 	{
 		case 0:
 			break;
@@ -121,7 +117,7 @@ void drawcube(int x, int y, int z, int c){
 		default:
 			break;
 	}
-
+	
 	if(c != 0){//if the colour is 0 don't draw solid cube
 		glPushMatrix();
 		glTranslatef(x,y,z);
@@ -156,15 +152,15 @@ void drawfirstlayer(){
 }
 
 //This function draws text in the game. The text indicates player's points, lives and his cubes
-void drawtext(float xpos, float zpos, float ypos, const char *string){
+void drawtext(float xPos, float zPos, float yPos, const char *string){
 	char *message = (char *)string;
 	int i =0;
 
-	glColor3f(1.0, 0.0, 0.0); //red
-	glRasterPos3f(xpos,ypos+0.2,zpos);
+	glColor3f(1.0, 1.0, 1.0); //red
+	glRasterPos3f(xPos,yPos+0.2,zPos); //set the message at this postition on the screen
 
 	while(*message != '\0'){
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *message++);
+		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *message++); //draw the text character by character
 	}
 }
 
@@ -253,62 +249,61 @@ void normal_keys(unsigned char key, int w, int h){;
 			break;
 
 		case 'W':
-		case 'w' :
-			if( (x+lx)<N && (z+lz)<N && (x+lx)>=0  && (z+lz)>=0){
-				if( ((C[x][z][y-1].getColour()) == 0)){
+		case 'w':
+			if( (x+lx)<N && (z+lz)<N && (x+lx)>=0  && (z+lz)>=0 ){
+				if( (C[x+lx][z+lz][y-1].getColour() == 0) && (C[x+lx][z+lz][y].getColour() == 0) ){// if there is no cube in front of you
+					player.points-=5*(y-2); //your points will be decreased by 5
+					while(y >= 1 && C[x+lx][z+lz][y-1].getColour() == 0) y--; //find the next available cube and touch down to this cube
 					x += lx;
 					z += lz;
-					player.points-=5*y;
-					y=1;
 				}
-				else{
-					if( (C[x+lx][z+lz][y].getColour()) == 0 ){//check if you have a cube in front of you and if not then go forward
+				else if( C[x+lx][z+lz][y].getColour() == 0 ){//check if you have a cube in front of you and if not then go forward
 						x += lx;
 						z += lz;	
-					}
-					else if( (C[x+lx][z+lz][y].getColour()) != 0){
+				}
+				else{ //else climb on the cube
 						x += lx;
 						z += lz;
 						y++;
-					}
 				}
 			}
+
 			checkTheBoxUnderMe();
 
-			if(y == N){
+			if(y == N){ //If the player reach the last layer, takes one life and 100 points
 				player.lives++;
 				player.points+=100;
 			}
 			break;
 
 		case 'A':
-		case 'a' :
-			temp = lx;
+		case 'a':
+			temp = lx; //I'm using this variable so as to exchange the values between lx & lz 
 			lx = lz;
 			lz = -temp;
 			break;
 
 		case 'D':
-		case 'd' :
-			temp = lx;
+		case 'd':
+			temp = lx; //I'm using this variable so as to exchange the values between lx & lz 
 			lx = -lz;
 			lz = temp;
 			break;
 
 		case 'S':
-		case 's' :
-			lx = -lx;
+		case 's':
+			lx = -lx;//reverse the values so as to look back
 			lz = -lz;
 			break;
 
 		case 'E':
 		case 'e':
-			if(C[x+lx][z+lz][y].getColour() != 0){
-				temp1 = 0;
+			if(C[x+lx][z+lz][y].getColour() != 0){//if there is a cube in front of player
+				cubesCounter = 0;//count the cubes
 
-				while(temp1<N){
-					C[x+lx][z+lz][y+temp1].eraseCube();
-					temp1++;
+				while(cubesCounter < N){
+					C[x+lx][z+lz][y+cubesCounter].eraseCube();//erase the cube on this layer
+					cubesCounter++;//go to the next layer
 				}
 
 				player.lives++;
@@ -322,14 +317,15 @@ void normal_keys(unsigned char key, int w, int h){;
 			break;
 
 		case (32) : //when the player push the space button the cubes are increased by 1 and he loses 5 points
+			C[x][z][y-1].setSpecificColour(player.cubes);//and the cube below changes colour
 			player.cubes++;
 			player.points-=5;
-			C[x][z][y-1].setSpecificColour(player.cubes);
 			break;
 
 		case 'R':
 		case 'r':
 			collapseAllCubes();
+			player.points+=10;
 			break;
 	}
 }
@@ -337,8 +333,8 @@ void normal_keys(unsigned char key, int w, int h){;
 void mouse_keys(int button, int state, int w, int h){
 	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
 		int i=0,count=0;
-		if(player.cubes>0 && (x+lx)<N && (z+lz)<N && (x+lx)>=0  && (z+lz)>=0){
-			while(i<N && count==0){
+		if(player.cubes>0 && ((x+lx)<N && (z+lz)<N && (x+lx)>=0  && (z+lz)>=0)){//check if the player has at least one cube to draw and if his not on the bounds
+			while(i<N && count==0){//if the player has a cube in front of him draw the cube above
 				if(C[x+lx][z+lz][y+i].getColour() == 0){
 					C[x+lx][z+lz][y+i].setColour();
 					player.cubes--;
@@ -351,15 +347,15 @@ void mouse_keys(int button, int state, int w, int h){
 			}
 		}
 	}
-	else if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN){
 
-		if( (x+lx)<N && (z+lz)<N && (x+lx)>=0  && (z+lz)>=0 ){
+	else if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN){
+		if( (x+lx)<N && (z+lz)<N && (x+lx)>=0  && (z+lz)>=0 ){//check if it's in the bounds
 			if(C[x+lx][z+lz][y].getColour() !=0 ){
 				int i=lx;
 				int j=lz;
 				int count=0;
 				
-				while((x+i)<N && (z+j)<N && (x+i)>=0  && (z+j)>=0 ){
+				while((x+i)<N && (z+j)<N && (x+i)>=0  && (z+j)>=0 ){//count how many cubes must be moved
 					if(C[x+i][z+j][y].getColour() != 0){
 						count++;
 						i+=lx;
@@ -368,14 +364,14 @@ void mouse_keys(int button, int state, int w, int h){
 					else break;
 				}
 
-				while((i!=lx || j!=lz) && count>0){
+				while((i!=lx || j!=lz) && count>0){ //move the cubes one by one
 					if((x+i)<N && (z+j)<N && (x+i)>=0  && (z+j)>=0){
 							if(C[x+i][z+j][y].getColour() == 0){
 								C[x+i][z+j][y].setSpecificColour(C[x+i-lx][z+j-lz][y].getColour());
 								C[x+i-lx][z+j-lz][y].eraseCube();
 							}
 					}
-					else {
+					else{
 						C[x+i-lx][z+j-lz][y].eraseCube();
 					}
 
@@ -407,12 +403,11 @@ int main(int argc,char* argv[]){
 		x=N/2;
 		z=N/2;
 	
-		//init
 		glutInit(&argc,argv);
 		glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 		glutInitWindowSize(650,650);
 		glutInitWindowPosition(100,100);
-		glutCreateWindow("Graphics Project 1");
+		glutCreateWindow("Cubes Game");
 	
 		glutDisplayFunc(display);
 		glutReshapeFunc(changeSize);
@@ -422,6 +417,7 @@ int main(int argc,char* argv[]){
 	
 		initializeCubeArray();
 		glEnable(GL_DEPTH_TEST);
+
 		glutMainLoop();
 	}
 	return 0;
